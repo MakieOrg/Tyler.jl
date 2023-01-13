7283c2b24d6cf440a5f41b12924bef3d900d13ce
Lazaro Alonso <lazarus.alon@gmail.com>
2023-01-13 16:37:25 +0100

    save

--- docs/examples/UserGuide/whale_ex.jl
+++ docs/examples/UserGuide/whale_ex.jl
@@ -5,48 +5,59 @@
 using Tyler, GLMakie
 using CSV, DataFrames
 using DataStructures: CircularBuffer
+using TileProviders
+using MapTiles
+
 mkpath("assets")#hide
+function to_web_mercator(lo,lat)
+    return Point2f(MapTiles.project((lo,lat), MapTiles.wgs84, MapTiles.web_mercator))
+end
 
-#m = Tyler.Map(Rect2f(-0.0921, 51.5, 0.04, 0.025))
-
-whale = CSV.read("./data/whale_shark_128786.csv", DataFrame)
+whale = CSV.read("./docs/examples/data/whale_shark_128786.csv", DataFrame)
 lon = whale[!, :lon]
 lat = whale[!, :lat]
 steps = size(lon,1)
 
+points = to_web_mercator.(lon,lat)
+
+lomn, lomx = extrema(lon)
+lamn, lamx = extrema(lat)
+δlon = abs(lomn - lomx)
+δlat = abs(lamn - lamx)
+
+set_theme!()
+
+provider = TileProviders.NASAGIBS(:ViirsEarthAtNight2012)
+m = Tyler.Map(Rect2f(lomn - δlon/2, lamn-δlat/2, 2δlon, 2δlat), 5;
+    provider, min_tiles=8, max_tiles=16)
+
 nt = 30
 trail = CircularBuffer{Point2f}(nt)
-fill!(trail, Point2f(lon[1], lat[1])) # add correct values to the circular buffer
+fill!(trail, points[1]) # add correct values to the circular buffer
 trail = Observable(trail) # make it an observable
-whale = Observable(Point2f(lon[1], lat[1]))
+whale = Observable(points[1])
 
 c = to_color(:dodgerblue)
 trailcolor = [RGBAf(c.r, c.g, c.b, (i/nt)^2.5) for i in 1:nt] # fading tail
+sleep(2)
 
-with_theme(theme_dark(), resolution = (1200,700)) do
-    fig = Figure()
-    ax = Axis(fig[1,1])
-    lines!(ax, trail; color = trailcolor, linewidth=3)
-    scatter!(ax, whale; ##image = collect(img_w), 
-        ##marker= Rect, markersize = reverse(size(img_w))./12,
-        ##rotations = rot
-        markersize = 15,
-        color = :orangered
-        )
-    limits!(ax, minimum(lon), maximum(lon), minimum(lat), maximum(lat))
-    ## the animation is done by updating the Observable values
-    ## change assets->(your folder) to make it work in your local env
-    record(fig, joinpath("assets", "whale_shark_128786.mp4"),
-        framerate = 16, profile = "main") do io
-        for i in 2:steps
-            push!(trail[], Point2f(lon[i], lat[i]))
-            whale[] = Point2f(lon[i],lat[i])
-            ## rot[] = Vec2f(lon[i-1]-lon[i], lat[i-1] - lat[i])
-            trail[] = trail[]
-            recordframe!(io)  # record a new frame
-        end
+objline = lines!(m.axis, trail; color = trailcolor, linewidth=3)
+objscatter = scatter!(m.axis, whale; markersize = 15, color = :orangered,
+    strokecolor=:grey90, strokewidth=1)
+hidedecorations!(m.axis)
+translate!(objline, 0, 0, 2)
+translate!(objscatter, 0, 0, 2)
+#limits!(ax, minimum(lon), maximum(lon), minimum(lat), maximum(lat))
+## the animation is done by updating the Observable values
+## change assets->(your folder) to make it work in your local env
+record(m.figure, joinpath("assets", "whale_shark_128786.mp4"),
+    framerate = 16, profile = "main") do io
+    for i in 2:steps
+        push!(trail[], points[i])
+        whale[] = points[i]
+        trail[] = trail[]
+        recordframe!(io)  # record a new frame
     end
-    nothing # hide
 end
 
 # !!! info
