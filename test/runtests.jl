49048b45187c800973fa072f67903ace5aca1aa1
SimonDanisch <sdanisch@protonmail.com>
2023-01-13 14:10:17 -0100

    update manifest

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,6 +20,15 @@ osm = graph_from_file("london_drive.json";
     graph_type=:light, # SimpleDiGraph
     weight_type=:distance
 )
+download_osm_buildings(:bbox;
+    area...,
+    metadata=true,
+    download_format=:osm,
+    save_to_file_location="london_buildings.osm"
+);
+
+# load as Buildings Dict
+buildings = buildings_from_file("london_buildings.osm");
 
 # Default
 london = Rect2f(-0.0921, 51.5, 0.04, 0.025)
@@ -27,11 +36,12 @@ m = Tyler.Map(london)
 
 # Nasa
 provider = TileProviders.NASAGIBS()
-m = Tyler.Map(Rect2f(0, 50, 40, 20), 5; provider=provider)
+m = Tyler.Map(Rect2f(0, 50, 40, 20), 5; provider=provider, min_tiles=8, max_tiles=32)
 
 # Google + OSM
 provider = TileProviders.Google(:satelite)
 m = Tyler.Map(london; provider=provider, coordinate_system=Tyler.wgs84)
 m.axis.aspect = map_aspect(area.minlat, area.maxlat)
-p = osmplot!(m.axis, osm)
+p = osmplot!(m.axis, osm; buildings)
+DataInspector(m.axis)
 translate!(p, 0, 0, 100)
