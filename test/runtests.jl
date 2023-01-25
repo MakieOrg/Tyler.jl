1c015cbc1e785044ff725f9fbbc700ee67020dd2
rafaqz <rafaelschouten@gmail.com>
2023-01-25 23:19:02 +0000

    example with just the current res and no layers

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using GLMakie
 
 # Default
 london = Rect2f(-0.0921, 51.5, 0.04, 0.025)
-m = wait(Tyler.Map(london; halo=0.0, depth=4)) # waits until all tiles are displayed
+m = wait(Tyler.Map(london; halo=0.0, depth=0)) # waits until all tiles are displayed
 @test isempty(m.tiles_being_added)
 @test isempty(m.queued_but_not_downloaded)
 @test length(m.displayed_tiles) == 162
