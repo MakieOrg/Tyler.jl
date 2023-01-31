7eb33d4142854ab939cfafeb48bc16964102a13a
rafaqz <rafaelschouten@gmail.com>
2023-01-31 10:02:28 +0000

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,8 @@ london = Rect2f(-0.0921, 51.5, 0.04, 0.025)
 m = wait(Tyler.Map(london)) # waits until all tiles are displayed
 @test isempty(m.tiles_being_added)
 @test isempty(m.queued_but_not_downloaded)
-@test length(m.displayed_tiles) == 162
-@test length(m.fetched_tiles) == 162
+@test length(m.displayed_tiles) == 48
+@test length(m.fetched_tiles) == 48
 
 # Reference tests?
 # provider = TileProviders.NASAGIBS()
