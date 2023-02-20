d8d500280597c81b439230fb0f74aca3967407bc
Rafael Schouten <rafaelschouten@gmail.com>
2023-02-20 11:24:56 +0100

    smooth zooming (#13)
    * add multi-layer display with halo
    
    * use figure resolution and add keyword
    
    * clean up show
    
    * update tests
    
    * fix test
    
    * example with less down load ;)
    
    * example with just the current res and no layers
    
    * fix halo
    
    * fix halo calculation
    
    * use 0.3 for default halo
    
    * use defaults in test
    
    * 0.2 halo
    
    * fix test
    
    * cleanup
    
    * add z_index for WGS84
    
    * clean up and fix docs
    
    * fix test
    
    * remove spiral experiment for now
    
    * add scale
    
    * fix tests
    
    * bugfix
    
    * f
    
    * remove min_tiles
    
    * Update Documenter.yml
    
    * Update make.jl
    
    ---------
    
    Co-authored-by: Simon <sdanisch@protonmail.com>

--- docs/make.jl
+++ docs/make.jl
@@ -20,7 +20,7 @@ makedocs(
     build=joinpath(@__DIR__, "docs")
 )
 
-deploydocs(; repo="https://github.com/MakieOrg/Tyler.jl.git", push_preview=true,
+deploydocs(; repo="github.com/MakieOrg/Tyler.jl.git", push_preview=true,
     deps=Deps.pip("mkdocs", "pygments", "python-markdown-math", "mkdocs-material",
         "pymdown-extensions", "mkdocstrings", "mknotebooks",
         "pytkdocs_tweaks", "mkdocs_include_exclude_files", "jinja2", "mkdocs-video"),
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,15 @@ london = Rect2f(-0.0921, 51.5, 0.04, 0.025)
 m = wait(Tyler.Map(london)) # waits until all tiles are displayed
 @test isempty(m.tiles_being_added)
 @test isempty(m.queued_but_not_downloaded)
-@test length(m.displayed_tiles) == 25
-@test length(m.fetched_tiles) == 25
+@test length(m.displayed_tiles) == 24
+@test length(m.fetched_tiles) == 24
+
+london = Rect2f(-0.0921, 51.5, 0.04, 0.025)
+m = wait(Tyler.Map(london; provider=Tyler.TileProviders.Google(), coordinate_system=Tyler.MapTiles.WGS84())) # waits until all tiles are displayed
+@test isempty(m.tiles_being_added)
+@test isempty(m.queued_but_not_downloaded)
+@test length(m.displayed_tiles) == 24
+@test length(m.fetched_tiles) == 24
 
 # Reference tests?
 # provider = TileProviders.NASAGIBS()
