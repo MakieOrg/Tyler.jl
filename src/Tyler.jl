2c6ad84f0c0235773b6570053fead528430580da
gaelforget <gforget@mit.edu>
2023-03-02 08:13:45 -0500

    use dispatch rather than if/else + allow user choice of colormap

--- src/Tyler.jl
+++ src/Tyler.jl
@@ -209,25 +209,33 @@ function create_tile_plot!(tyler::Map, tile::Tile, image::TileImage)
     place_tile!(tile, mplot, tyler.coordinate_system)
 end
 
-cols=to_colormap(:thermal)
-col(i)=RGBAf(Makie.interpolated_getindex(cols,i))
+##
 
 function fetch_tile(tyler::Map, tile::Tile)
     return get!(tyler.fetched_tiles, tile) do
-    if isa(tyler.provider,Interpolator)
-        itp=tyler.provider.url
-        (lon,lat) = tile2positions(tile)
-        z = permutedims(itp.(lon,lat))
-        return [col(i) for i in z]
-    elseif isa(tyler.provider,AbstractProvider)
-        url = TileProviders.geturl(tyler.provider, tile.x, tile.y, tile.z)
-        result = HTTP.get(url; retry=false, readtimeout=4, connect_timeout=4)
-        return ImageMagick.readblob(result.body)
-    else
-        error("unknown provider type")
-    end
+        fetch_tile(tyler.provider,tile)
     end
 end
+
+function fetch_tile(provider::AbstractProvider, tile::Tile)
+    url = TileProviders.geturl(provider, tile.x, tile.y, tile.z)
+    result = HTTP.get(url; retry=false, readtimeout=4, connect_timeout=4)
+    return ImageMagick.readblob(result.body)
+end
+
+cols=Makie.to_colormap(:thermal)
+col(i)=RGBAf(Makie.interpolated_getindex(cols,i))
+col(i::RGBAf)=i
+
+function fetch_tile(provider::Interpolator, tile::Tile)
+    itp=provider.url
+    (lon,lat) = tile2positions(tile)
+    z = permutedims(itp.(lon,lat))
+    return [col(i) for i in z]
+end
+
+
+##
 
 function queue_tile!(tyler::Map, tile)
     queue = tyler.tiles_being_added
