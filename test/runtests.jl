912b69cbed45445471af1e2f927017164d9b2041
rafaqz <rafaelschouten@gmail.com>
2023-04-29 20:18:35 +0200

    test crs and extent in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using Test
 using Tyler
 using GLMakie
 using Extents
+using GeoInterface
 
 # Default
 london = Rect2f(-0.0921, 51.5, 0.04, 0.025)
@@ -21,7 +22,21 @@ m = wait(Tyler.Map(london; provider=Tyler.TileProviders.Google(), crs=Tyler.MapT
 # test Extent input
 london = Extents.Extent(X=(-0.0921, -0.0521), Y=(51.5, 51.525))
 m = wait(Tyler.Map(london)) # waits until all tiles are displayed
+@test isempty(m.tiles_being_added)
+@test isempty(m.queued_but_not_downloaded)
 @test length(m.displayed_tiles) == 24
+@test length(m.fetched_tiles) == 24
+
+@testset "Interfaces" begin
+    from = Tyler.MapTiles.WebMercator()
+    to = Tyler.MapTiles.WGS84()
+    # Uncomment when ≈ works in Extents.jl
+    # @test map(zip(Extents.extent(m)...)) do b
+    #     Tyler.MapTiles.project(b, from, to) 
+    # end ≈ Extents.extent(london)
+    @test Extents.extent(m) isa Extents.Extent
+    @test GeoInterface.crs(m) == Tyler.MapTiles.WebMercator()
+end
 
 # Reference tests?
 # provider = TileProviders.NASAGIBS()
