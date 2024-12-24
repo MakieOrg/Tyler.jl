e11ab3695656763eb7237767ad49fc7c1224c4a3
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-12-24 21:53:49 +0530

    try more adjustments

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,16 +40,18 @@ end
 
 @testset "NamedTuple axis syntax" begin
     b = Rect2f(-20.0, -20.0, 40.0, 40.0)
-    m = @test_nowarn Tyler.Map(b, provider=Tyler.TileProviders.OpenStreetMap(), axis = (; type = Axis, aspect = AxisAspect(1)))
-    @test only(contents(m.figure.layout[1, 1])) isa Axis
-    @test only(contents(m.figure.layout[1, 1])).aspect[] == AxisAspect(1)
+    m1 = @test_nowarn Tyler.Map(b, provider=Tyler.TileProviders.OpenStreetMap(), axis = (; type = Axis, aspect = AxisAspect(1)))
+    @test only(contents(m1.figure.layout[1, 1])) isa Axis
+    @test only(contents(m1.figure.layout[1, 1])).aspect[] == AxisAspect(1)
+    close(m1)
 end
 
 @testset "Pass GridPosition to figure kwarg" begin
     b = Rect2f(-20.0, -20.0, 40.0, 40.0)
     f = Figure()
-    m = @test_nowarn Tyler.Map(b, figure = f[1, 2])
-    @test only(contents(m.figure.layout[1, 2])) isa Axis
+    m1 = @test_nowarn Tyler.Map(b, figure = f[1, 2])
+    @test only(contents(m1.figure.layout[1, 2])) isa Axis
+    close(m1)
 end
 
 # Reference tests?
