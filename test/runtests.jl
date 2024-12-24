8369ac4a05d5185ec2437226fd206e2027a3e753
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-12-24 09:27:22 +0530

    Fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -42,7 +42,7 @@ end
     b = Rect2f(-20.0, -20.0, 40.0, 40.0)
     m = @test_nowarn Tyler.Map(b, provider=Tyler.TileProviders.OpenStreetMap(), axis = (; type = Axis, aspect = AxisAspect(1)))
     @test only(contents(m.figure.layout[1, 1])) isa Axis
-    @test only(contents(m.figure.layout[1, 1])).aspect == AxisAspect(1)
+    @test only(contents(m.figure.layout[1, 1])).aspect[] == AxisAspect(1)
 end
 
 @testset "Pass GridPosition to figure kwarg" begin
